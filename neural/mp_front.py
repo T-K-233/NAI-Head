36e49861bd427a10d6067295d648f8df658118eb
-T.K.- <t_k_233@outlook.com>
2024-12-16 22:04:28 -0800

    ADD: update MP front

--- neural/mp_front.py
+++ neural/mp_front.py
@@ -5,18 +5,24 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-while True:
-    cap = cv2.VideoCapture(0)
-    with mp_hands.Hands(
-        model_complexity=0,
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as hands:
-        while cap.isOpened():
-            success, image = cap.read()
-            if not success:
-                print("Ignoring empty camera frame.")
+print("Starting camera")
+cap = cv2.VideoCapture(0)
+
+with mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+
+    print("Camera started")
+        
+    while cap.isOpened():
+        print("Reading frame")
+        success, image = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+            exit()
         # If loading a video, use 'break' instead of 'continue'.
-                continue
+            
 
         # To improve performance, optionally mark the image as not writeable to
         # pass by reference.
@@ -37,7 +43,7 @@ while True:
                 mp_drawing_styles.get_default_hand_landmarks_style(),
                 mp_drawing_styles.get_default_hand_connections_style())
         # Flip the image horizontally for a selfie-view display.
-        cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+        cv2.imshow("MediaPipe Hands", cv2.flip(image, 1))
         if cv2.waitKey(5) & 0xFF == 27:
             break
     cap.release()
